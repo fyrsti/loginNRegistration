ca4310832ad83810b214ffb84ef25f08f31417a7
fyrsti <wonderdeveloper@gmail.com>
2021-02-24 22:24:29 +0200

    Initialize project with some start functions

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+
+using namespace std;
+
+enum CMD_CODE
+{
+	EXIT,
+	HELP,
+	REGISTER,
+	LOGIN
+};
+
+map<string, int> cmd_translator{
+	pair<string, int>("/help", HELP),
+	pair<string, int> ("/l", LOGIN),
+	pair<string, int> ("/r", REGISTER),
+	pair<string, int>("/exit", EXIT),
+	pair<string, int>("/q", EXIT)
+};
+
+template <typename T>
+void input(string text, T& destination);
+
+
+bool command_exist(string cmd)
+{
+	return (cmd_translator.find(cmd) != cmd_translator.end() ? true : false);
+}
+
+
+void guest_controller()
+{
+	string command;
+	while (true)
+	{
+		input(">>> ", command);
+		if (command_exist(command))
+		{
+			switch (cmd_translator.at(command))
+			{
+			case EXIT:
+				exit(0);
+				break;
+			case HELP:
+				cout << "Not implemented!" << endl;
+				break;
+			case REGISTER:
+				cout << "Not implemented!" << endl;
+				break;
+			case LOGIN:
+				cout << "Not implemented!" << endl;
+				break;
+			}
+		}
+		else
+			cout << "Incorrect command!" << endl;
+	}
+}
+
+
+template <typename T>
+void input(string text, T& destination)
+{
+	cout << text;
+	cin >> destination;
+}
+
+
+int main()
+{
+	guest_controller();
+	return 0;
+}
