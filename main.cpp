eca1ba12576920de06fba8e1962952e3442dac8e
fyrsti <wonderdeveloper@gmail.com>
2021-02-25 19:12:01 +0200

    User controller was created with some logic.

--- main.cpp
+++ main.cpp
@@ -8,25 +8,79 @@ using namespace std;
 
 template <typename T>
 void input(string text, T& destination);
+void user_controller(string username);
 
 enum CMD_CODE
 {
 	EXIT,
 	HELP,
 	REGISTER,
-	LOGIN
+	LOGIN,
+	LOGOUT,
+	CHANGEPASS
 };
 
 map<string, int> cmd_translator{
 	pair<string, int>("/help", HELP),
-	pair<string, int> ("/l", LOGIN),
-	pair<string, int> ("/r", REGISTER),
+	pair<string, int>("/l", LOGIN),
+	pair<string, int>("/r", REGISTER),
 	pair<string, int>("/exit", EXIT),
-	pair<string, int>("/q", EXIT)
+	pair<string, int>("/q", EXIT),
+	pair<string, int>("/logout", LOGOUT),
+	pair<string, int>("/changepassword", CHANGEPASS)
 };
 
 
 map<string, string> db;
+
+
+bool _verify_parameters(string LU, string LP)
+{
+	if (db.find(LU) != db.end())
+	{
+		if (db.at(LU) == LP)
+		{
+			cout << " ---- Success ---- " << endl;
+			return true;
+		}
+		cout << "Incorrect password!" << endl;
+		return false;
+	}
+	else
+	{
+		cout << "There's no account with this username." << endl;
+		return false;
+	}
+}
+
+
+string _get_login_parameter(string text)
+{
+	string returned;
+	input(text, returned);
+	if (returned == "/back")
+		return "0";
+	else
+		return returned;
+}
+
+
+
+void cmd_login()
+{
+	string LUsername, LPassword;
+	while (true)
+	{
+		LUsername = _get_login_parameter("Login username: ");
+		if (LUsername == "0")
+			return;
+		LPassword = _get_login_parameter("Login password: ");
+		if (LPassword != "0")
+			break;
+	}
+	if (_verify_parameters(LUsername, LPassword))
+		user_controller(LUsername);
+}
 
 
 string _get_username()
@@ -100,6 +154,40 @@ bool command_exist(string cmd)
 	return (cmd_translator.find(cmd) != cmd_translator.end() ? true : false);
 }
 
+
+void user_controller(string username)
+{
+	system("cls");
+	cout << "You are welcome, " << username << endl;
+	string command;
+	while (true)
+	{
+		input(username + ">>> ", command);
+		if (command_exist(command))
+		{
+			switch (cmd_translator.at(command))
+			{
+			case EXIT:
+				exit(0);
+			case HELP:
+				cmd_help({ LOGOUT, CHANGEPASS });
+				break;
+			case LOGOUT:
+				cout << "Not implemented!" << endl;
+				break;
+			case CHANGEPASS:
+				cout << "Not implemented!" << endl;
+				break;
+			default:
+				cout << "Incorrect command!" << endl;
+			}
+		}
+		else
+			cout << "Incorrect command!" << endl;
+	}
+}
+
+
 void guest_controller()
 {
 	string command;
@@ -112,7 +200,6 @@ void guest_controller()
 			{
 			case EXIT:
 				exit(0);
-				break;
 			case HELP:
 				cmd_help(vector<int>{REGISTER, LOGIN, EXIT});
 				break;
@@ -120,8 +207,10 @@ void guest_controller()
 				cmd_register();
 				break;
 			case LOGIN:
-				cout << "Not implemented!" << endl;
+				cmd_login();
 				break;
+			default:
+				cout << "Incorrect command!" << endl;
 			}
 		}
 		else
